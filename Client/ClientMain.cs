d12d246dfeaaed2ada08561cebce018d22af4928
░L░E░O░ <83369389+devmailar@users.noreply.github.com>
2025-04-11 18:53:30 +0300

    Update PolyZone.cs

--- Client/ClientMain.cs
+++ Client/ClientMain.cs
@@ -1,22 +1,64 @@
 using CitizenFX.Core;
+using System.Drawing;
+using System.Threading.Tasks;
 
 namespace PolyZoneLib.Client
 {
     internal class ClientMain : BaseScript
     {
+        private bool IsRecording = false;
+        private Vector2 PrevPos = new Vector2(0, 0);
+
         public ClientMain()
         {
-            Debug.WriteLine("Hi from PolyZoneLib.Client!");
-
-            var test = new PolyZone(new Vector2[]
+            var testZone = new PolyZone(new Vector2[]
             {
-                new Vector2(-1916.43f, -708.24f),
-                new Vector2(-1919.53f, -705.05f),
-                new Vector2(-1922.82f, -709.64f),
-                new Vector2(-1919.58f, -711.68f)
-            });
+                new Vector2(-1858.797f, -632.3256f),
+                new Vector2(-1860.446f, -630.6107f),
+                new Vector2(-1863.622f, -627.6506f),
+                new Vector2(-1862.642f, -626.3374f),
+                new Vector2(-1859.648f, -623.0164f),
+                new Vector2(-1856.58f, -619.6808f),
+                new Vector2(-1854.122f, -617.1497f),
+                new Vector2(-1851.67f, -619.1915f),
+                new Vector2(-1848.887f, -621.5549f),
+                new Vector2(-1851.483f, -624.6349f),
+                new Vector2(-1854.477f, -628.041f),
+                new Vector2(-1857.334f, -631.3133f),
+                new Vector2(-1858.548f, -632.9578f)
+            }, 10.30f, 13.30f, Color.FromArgb(180, 11, 55, 123));
 
-            Tick += test.Draw;
+            Tick += testZone.Draw;
+        }
+
+        public async Task OnRecord()
+        {
+            if (PrevPos == new Vector2(Game.PlayerPed.Position.X, Game.PlayerPed.Position.Y))
+            {
+                return;
+            }
+
+            PrevPos = new Vector2(Game.PlayerPed.Position.X, Game.PlayerPed.Position.Y);
+
+            Debug.WriteLine($"new Vector2({Game.PlayerPed.Position.X}f, {Game.PlayerPed.Position.Y}f),");
+
+            await Delay(2500);
+        }
+
+        [Command("record")]
+        public void Record()
+        {
+            IsRecording = !IsRecording;
+
+            if (!IsRecording)
+            {
+                Debug.WriteLine("RECORDING STOPPED");
+                Tick -= OnRecord;
+                return;
+            }
+
+            Debug.WriteLine("RECORDING STARTED");
+            Tick += OnRecord;
         }
     }
 }
--- Client/PolyZone.cs
+++ Client/PolyZone.cs
@@ -8,12 +8,16 @@ namespace PolyZoneLib.Client
     internal class PolyZone
     {
         private readonly List<Vector2> Points = new List<Vector2>();
-        private readonly float CeilingZ = 10.73f;
-        private readonly float FloorZ = 5.61f;
+        private readonly float Ceiling;
+        private readonly float Floor;
+        private readonly Color Color;
 
-        internal PolyZone(Vector2[] points)
+        internal PolyZone(Vector2[] points, float floor, float ceiling, Color color)
         {
             Points.AddRange(points);
+            Floor = floor;
+            Ceiling = ceiling;
+            Color = color;
         }
 
         internal async Task Draw()
@@ -26,67 +30,61 @@ namespace PolyZoneLib.Client
             var vertices = new List<Vector3>();
             foreach (var point in Points)
             {
-                vertices.Add(new Vector3(point.X, point.Y, CeilingZ));
+                vertices.Add(new Vector3(point.X, point.Y, Ceiling));
             }
 
-            var center = new Vector3(Points[0].X, Points[0].Y, CeilingZ);
+            var center = new Vector3(Points[0].X, Points[0].Y, Ceiling);
 
             for (var i = 0; i < Points.Count; i++)
             {
                 Vector2 startPoint2D = Points[i];
                 Vector2 endPoint2D = Points[(i + 1) % Points.Count];
 
-                Vector3 startPoint3D = new Vector3(startPoint2D.X, startPoint2D.Y, CeilingZ);
-                Vector3 endPoint3D = new Vector3(endPoint2D.X, endPoint2D.Y, CeilingZ);
+                Vector3 startPoint3D = new Vector3(startPoint2D.X, startPoint2D.Y, Ceiling);
+                Vector3 endPoint3D = new Vector3(endPoint2D.X, endPoint2D.Y, Ceiling);
 
-                World.DrawLine(startPoint3D, endPoint3D, Color.FromArgb(200, 255, 30, 68));
+                World.DrawLine(startPoint3D, endPoint3D, Color);
 
-                Vector3 ceiling = new Vector3(Points[i].X, Points[i].Y, CeilingZ);
-                Vector3 floor = new Vector3(Points[i].X, Points[i].Y, FloorZ);
+                Vector3 ceiling = new Vector3(Points[i].X, Points[i].Y, Ceiling);
+                Vector3 floor = new Vector3(Points[i].X, Points[i].Y, Floor);
 
-                World.DrawLine(ceiling, floor, Color.FromArgb(200, 255, 30, 68));
+                World.DrawLine(ceiling, floor, Color);
             }
 
-            // Fill ceiling
             for (int i = 1; i < vertices.Count - 1; i++)
             {
                 Vector3 p1 = center;
                 Vector3 p2 = vertices[i];
                 Vector3 p3 = vertices[i + 1];
 
-                World.DrawPoly(p1, p2, p3, Color.FromArgb(200, 255, 30, 68));
-                World.DrawPoly(p2, p1, p3, Color.FromArgb(200, 255, 30, 68));
+                World.DrawPoly(p1, p2, p3, Color);
+                World.DrawPoly(p2, p1, p3, Color);
             }
 
-            // Fill floor
             for (int i = 1; i < vertices.Count - 1; i++)
             {
-                Vector3 p1 = new Vector3(Points[0].X, Points[0].Y, FloorZ);
-                Vector3 p2 = new Vector3(Points[i].X, Points[i].Y, FloorZ);
-                Vector3 p3 = new Vector3(Points[i + 1].X, Points[i + 1].Y, FloorZ);
+                Vector3 p1 = new Vector3(Points[0].X, Points[0].Y, Floor);
+                Vector3 p2 = new Vector3(Points[i].X, Points[i].Y, Floor);
+                Vector3 p3 = new Vector3(Points[i + 1].X, Points[i + 1].Y, Floor);
 
-                World.DrawPoly(p1, p2, p3, Color.FromArgb(200, 255, 30, 68));
-                World.DrawPoly(p2, p1, p3, Color.FromArgb(200, 255, 30, 68));
+                World.DrawPoly(p1, p2, p3, Color);
+                World.DrawPoly(p2, p1, p3, Color);
             }
 
-            // Fill vertical walls (double-sided)
             for (var i = 0; i < Points.Count; i++)
             {
                 Vector2 startPoint2D = Points[i];
                 Vector2 endPoint2D = Points[(i + 1) % Points.Count];
 
-                Vector3 ceiling1 = new Vector3(startPoint2D.X, startPoint2D.Y, CeilingZ);
-                Vector3 ceiling2 = new Vector3(endPoint2D.X, endPoint2D.Y, CeilingZ);
-                Vector3 floor1 = new Vector3(startPoint2D.X, startPoint2D.Y, FloorZ);
-                Vector3 floor2 = new Vector3(endPoint2D.X, endPoint2D.Y, FloorZ);
+                Vector3 ceiling1 = new Vector3(startPoint2D.X, startPoint2D.Y, Ceiling);
+                Vector3 ceiling2 = new Vector3(endPoint2D.X, endPoint2D.Y, Ceiling);
+                Vector3 floor1 = new Vector3(startPoint2D.X, startPoint2D.Y, Floor);
+                Vector3 floor2 = new Vector3(endPoint2D.X, endPoint2D.Y, Floor);
 
-                // Draw front-facing wall
-                World.DrawPoly(ceiling1, floor1, floor2, Color.FromArgb(150, 255, 30, 68));
-                World.DrawPoly(floor2, ceiling2, ceiling1, Color.FromArgb(150, 255, 30, 68));
-
-                // Draw reverse-facing wall
-                World.DrawPoly(floor1, ceiling1, floor2, Color.FromArgb(150, 255, 30, 68));
-                World.DrawPoly(ceiling2, floor2, ceiling1, Color.FromArgb(150, 255, 30, 68));
+                World.DrawPoly(ceiling1, floor1, floor2, Color);
+                World.DrawPoly(floor2, ceiling2, ceiling1, Color);
+                World.DrawPoly(floor1, ceiling1, floor2, Color);
+                World.DrawPoly(ceiling2, floor2, ceiling1, Color);
             }
 
             await Task.FromResult(0);
